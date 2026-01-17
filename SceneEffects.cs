27a58520a7b07a655a982596b08e46a18802e6b3
2249438322 <2249438322@qq.com>
2026-01-18 01:11:53 +0800

    统一并规范化所有音频文件

--- SceneEffects.cs
+++ SceneEffects.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Terraria;
 using Terraria.ModLoader;
-using Terraria;
 using tModPorter;
 
 namespace ArknightsModMusic
@@ -21,110 +16,127 @@ namespace ArknightsModMusic
 
     // --- 沙漠 ---
     public class Music_DesertDay : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Gareth Coker - 细沙";
+        // 细沙
+        public override string FileName => "DesertDay";
         public override bool IsEnabled => Config.EnableDesertDay;
         public override bool IsSceneEffectActive(Player p) => p.ZoneDesert && Main.dayTime;
     }
 
     public class Music_DesertNight : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Gareth Coker - From The Desert";
+        // From The Desert
+        public override string FileName => "DesertNight";
         public override bool IsEnabled => Config.EnableDesertNight;
         public override bool IsSceneEffectActive(Player p) => p.ZoneDesert && !Main.dayTime;
     }
 
     public class Music_DesertUnderground : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Eagle Wei - 烈阳与刀刃";
+        // 烈阳与刀刃
+        public override string FileName => "DesertUnderground";
         public override bool IsEnabled => Config.EnableDesertUnderground;
         public override bool IsSceneEffectActive(Player p) => p.ZoneUndergroundDesert;
     }
 
     // --- 丛林 ---
     public class Music_JungleDay : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR - 绿洲";
+        // 绿洲
+        public override string FileName => "JungleDay";
         public override bool IsEnabled => Config.EnableJungleDay;
         public override bool IsSceneEffectActive(Player p) => p.ZoneJungle && Main.dayTime;
     }
 
     public class Music_JungleNight : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & KK ZHANG - 逐猎";
+        // 逐猎
+        public override string FileName => "JungleNight";
         public override bool IsEnabled => Config.EnableJungleNight;
         public override bool IsSceneEffectActive(Player p) => p.ZoneJungle && !Main.dayTime;
     }
 
     public class Music_JungleUnderground : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Eagle Wei - 恐惧暗潮";
+        // 恐惧暗潮
+        public override string FileName => "JungleUnderground";
         public override bool IsEnabled => Config.EnableJungleUnderground;
         public override bool IsSceneEffectActive(Player p) => p.ZoneJungle && p.ZoneDirtLayerHeight;
     }
 
     // --- 雪地 ---
     public class Music_SnowDay : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Adam Gubman - 因非冰原";
+        // 因非冰原
+        public override string FileName => "SnowDay";
         public override bool IsEnabled => Config.EnableSnowDay;
         public override bool IsSceneEffectActive(Player p) => p.ZoneSnow && Main.dayTime;
     }
 
     public class Music_SnowNight : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Adam Gubman - 远见终界";
+        // 远见终界
+        public override string FileName => "SnowNight";
         public override bool IsEnabled => Config.EnableSnowNight;
         public override bool IsSceneEffectActive(Player p) => p.ZoneSnow && !Main.dayTime;
     }
 
     public class Music_SnowUnderground : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Adam Gubman - 归还荒野";
+        // 归还荒野
+        public override string FileName => "SnowUnderground";
         public override bool IsEnabled => Config.EnableSnowUnderground;
         public override bool IsSceneEffectActive(Player p) => p.ZoneSnow && p.ZoneRockLayerHeight;
     }
 
     // --- 神圣 ---
     public class Music_HallowDay : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Erik Castro - 不存在的乐园";
+        // 不存在的乐园
+        public override string FileName => "HallowDay";
         public override bool IsEnabled => Config.EnableHallowDay;
         public override bool IsSceneEffectActive(Player p) => p.ZoneHallow && Main.dayTime;
     }
 
     public class Music_HallowNight : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Erik Castro - 圣城日常";
+        // 圣城日常
+        public override string FileName => "HallowNight";
         public override bool IsEnabled => Config.EnableHallowNight;
         public override bool IsSceneEffectActive(Player p) => p.ZoneHallow && !Main.dayTime;
     }
 
     public class Music_HallowUnderground : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Erik Castro - 无罪之人";
+        // 无罪之人
+        public override string FileName => "HallowUnderground";
         public override bool IsEnabled => Config.EnableHallowUnderground;
         public override bool IsSceneEffectActive(Player p) => p.ZoneHallow && p.ZoneRockLayerHeight;
     }
 
     // --- 腐化 ---
     public class Music_CorruptSurface : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR - 荣光暗沉";
+        // 荣光暗沉
+        public override string FileName => "CorruptSurface";
         public override bool IsEnabled => Config.EnableCorruptSurface;
         public override bool IsSceneEffectActive(Player p) => p.ZoneCorrupt && p.ZoneOverworldHeight;
     }
 
     public class Music_CorruptUnderground : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & LJCH - Wecgas fore tham Cynge, Searu fore tham Ethle";
+        // Wecgas fore tham Cynge, Searu fore tham Ethle
+        public override string FileName => "CorruptUnderground";
         public override bool IsEnabled => Config.EnableCorruptUnderground;
         public override bool IsSceneEffectActive(Player p) => p.ZoneCorrupt && p.ZoneRockLayerHeight;
     }
 
     // --- 太空与地狱 ---
     public class Music_SpaceDay : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Steven Grove - Ad astra";
+        // Ad astra
+        public override string FileName => "SpaceDay";
         public override bool IsEnabled => Config.EnableSpaceDay;
         public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
         public override bool IsSceneEffectActive(Player p) => p.ZoneSkyHeight && Main.dayTime;
     }
 
     public class Music_SpaceNight : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Eagle Wei - 群星见我";
+        // 群星见我
+        public override string FileName => "SpaceNight";
         public override bool IsEnabled => Config.EnableSpaceNight;
         public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
         public override bool IsSceneEffectActive(Player p) => p.ZoneSkyHeight && !Main.dayTime;
     }
 
     public class Music_Hell : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR - Wavering Flame";
+        // Wavering Flame
+        public override string FileName => "Hell";
         public override bool IsEnabled => Config.EnableHell;
         public override SceneEffectPriority Priority => SceneEffectPriority.Environment;
         public override bool IsSceneEffectActive(Player p) => p.ZoneUnderworldHeight;
@@ -132,72 +144,83 @@ namespace ArknightsModMusic
 
     // --- 特殊事件 ---
     public class Music_BloodMoon : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Adam Gubman - 行文之韵";
+        // 行文之韵
+        public override string FileName => "BloodMoon";
         public override bool IsEnabled => Config.EnableBloodMoon;
         public override SceneEffectPriority Priority => SceneEffectPriority.Event;
         public override bool IsSceneEffectActive(Player p) => Main.bloodMoon;
     }
 
     public class Music_Meteor : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Steven Grove - 深层迷醉";
+        // 深层迷醉
+        public override string FileName => "Meteor";
         public override bool IsEnabled => Config.EnableMeteor;
         public override bool IsSceneEffectActive(Player p) => p.ZoneMeteor;
     }
 
     public class Music_Graveyard : SceneMusicLoaden {
-        public override string FileName => "塞壬唱片-MSR & Adam Gubman - 熏陶";
+        // 熏陶
+        public override string FileName => "Graveyard";
         public override bool IsEnabled => Config.EnableGraveyard;
         public override bool IsSceneEffectActive(Player p) => p.ZoneGraveyard;
     }
     // --- 腐化海洋 ---
     public class Music_CorruptedOceanDay : SceneMusicLoaden {
-        public override string FileName => "darktides";
+        // darktides
+        public override string FileName => "CorruptedOceanDay";
         public override bool IsEnabled => Config.EnableArknightsCorruptedOceanDaytime;
         public override bool IsSceneEffectActive(Player p) => IsAtOceanEdge(p) && Main.dayTime && p.ZoneCorrupt && !p.ZoneCrimson && !p.ZoneHallow;
     }
 
     public class Music_CorruptedOceanNight : SceneMusicLoaden {
-        public override string FileName => "spread";
+        // spread
+        public override string FileName => "CorruptedOceanNight";
         public override bool IsEnabled => Config.EnableArknightsCorruptedOceanNighttime;
         public override bool IsSceneEffectActive(Player p) => IsAtOceanEdge(p) && !Main.dayTime && p.ZoneCorrupt && !p.ZoneCrimson && !p.ZoneHallow;
     }
 
     // --- 血腥海洋 ---
     public class Music_CrimsonOceanDay : SceneMusicLoaden {
-        public override string FileName => "resonance";
+        // resonance
+        public override string FileName => "CrimsonOceanDay";
         public override bool IsEnabled => Config.EnableArknightsCrimsonOceanDaytime;
         public override bool IsSceneEffectActive(Player p) => IsAtOceanEdge(p) && Main.dayTime && !p.ZoneCorrupt && p.ZoneCrimson && !p.ZoneHallow;
     }
 
     public class Music_CrimsonOceanNight : SceneMusicLoaden {
-        public override string FileName => "eternalreef";
+        // eternalreef
+        public override string FileName => "CrimsonOceanNight";
         public override bool IsEnabled => Config.EnableArknightsCrimsonOceanNighttime;
         public override bool IsSceneEffectActive(Player p) => IsAtOceanEdge(p) && !Main.dayTime && !p.ZoneCorrupt && p.ZoneCrimson && !p.ZoneHallow;
     }
 
     // --- 神圣海洋 ---
     public class Music_HallowedOceanDay : SceneMusicLoaden {
-        public override string FileName => "pathoflife";
+        // "pathoflife"
+        public override string FileName => "HallowedOceanDay";
         public override bool IsEnabled => Config.EnableArknightsHallowedOceanDaytime;
         public override bool IsSceneEffectActive(Player p) => IsAtOceanEdge(p) && Main.dayTime && !p.ZoneCorrupt && !p.ZoneCrimson && p.ZoneHallow;
     }
 
     public class Music_HallowedOceanNight : SceneMusicLoaden {
-        public override string FileName => "upsidedowntreeshadow";
+        // "upsidedowntreeshadow"
+        public override string FileName => "HallowedOceanNight";
         public override bool IsEnabled => Config.EnableArknightsHallowedOceanNighttime;
         public override bool IsSceneEffectActive(Player p) => IsAtOceanEdge(p) && !Main.dayTime && !p.ZoneCorrupt && !p.ZoneCrimson && p.ZoneHallow;
     }
 
     // --- 普通海洋 ---
     public class Music_OceanDay : SceneMusicLoaden {
-        public override string FileName => "ready";
+        // "ready"
+        public override string FileName => "OceanDay";
         public override bool IsEnabled => Config.EnableArknightsOceanDaytime;
         public override SceneEffectPriority Priority => SceneEffectPriority.BiomeLow;
         public override bool IsSceneEffectActive(Player p) => p.active && p.ZoneBeach && Main.dayTime && p.ZoneOverworldHeight && !p.ZoneCorrupt && !p.ZoneCrimson;
     }
 
     public class Music_OceanNight : SceneMusicLoaden {
-        public override string FileName => "seawonder";
+        // "seawonder"
+        public override string FileName => "OceanNight";
         public override bool IsEnabled => Config.EnableArknightsOceanNighttime;
         public override SceneEffectPriority Priority => SceneEffectPriority.BiomeLow;
         public override bool IsSceneEffectActive(Player p) => p.active && p.ZoneBeach && !Main.dayTime && p.ZoneOverworldHeight && !p.ZoneCorrupt && !p.ZoneCrimson;
@@ -205,14 +228,16 @@ namespace ArknightsModMusic
 
     // --- 森林 ---
     public class Music_ForestDay : SceneMusicLoaden {
-        public override string FileName => "lifeglow";
+        // "lifeglow"
+        public override string FileName => "ForestDay";
         public override bool IsEnabled => Config.EnableArknightsForestDaytime;
         public override SceneEffectPriority Priority => SceneEffectPriority.BiomeLow;
         public override bool IsSceneEffectActive(Player p) => p.active && p.ZoneForest && Main.dayTime && p.ZoneOverworldHeight && !p.ZoneDesert && !p.ZoneBeach && !p.ZoneHallow;
     }
 
     public class Music_ForestNight : SceneMusicLoaden {
-        public override string FileName => "asisaw";
+        // "asisaw"
+        public override string FileName => "ForestNight";
         public override bool IsEnabled => Config.EnableArknightsForestNighttime;
         public override SceneEffectPriority Priority => SceneEffectPriority.BiomeLow;
         public override bool IsSceneEffectActive(Player p) => p.active && p.ZoneForest && !Main.dayTime && p.ZoneOverworldHeight && !p.ZoneDesert && !p.ZoneBeach && !p.ZoneHallow;
